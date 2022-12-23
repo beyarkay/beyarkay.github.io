8ca8c07fa621dc867b79e7b469046dfb299b6232
beyarkay <boydrkane@gmail.com>
2022-12-23 13:53:31 +0200

    Shuffle root components

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,25 @@ import Container from "@mui/material/Container"
 import FindCalendar from "./FindCalendar"
 import Header from "./Header"
 import MakePullRequest from "./MakePullRequest"
-import {Link} from "@mui/material"
+import {Link, Typography} from "@mui/material"
 
 type View = "findCalendar" | "makePullRequest";
 
 function App() {
-    const [view, setView] = React.useState<View>("makePullRequest")
+    const [view, setView] = React.useState<View>("findCalendar")
 
     const setViewOnClick = () => setView(
         view === "findCalendar" ? "makePullRequest" : "findCalendar"
     )
 
     return (<>
-        <Container maxWidth="lg">
-            <Header />
+        <Header />
+        <Typography variant="subtitle2" align="center">
             <Link onClick={setViewOnClick}>
                 {view === "findCalendar" ? "make pull request" : " find your calendar"}
             </Link>
-            {view === "findCalendar" ? <FindCalendar /> : <MakePullRequest />}
-        </Container>
+        </Typography>
+        {view === "findCalendar" ? <FindCalendar /> : <MakePullRequest />}
     </>)
 }
 
