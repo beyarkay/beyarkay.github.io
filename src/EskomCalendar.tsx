c77177b904750fb787a8c9f3c88e857da9993c0e
Luc Hayward <luchay10@gmail.com>
2023-04-07 16:44:42 +0200

    Remove whitespace changes

--- src/EskomCalendar.tsx
+++ src/EskomCalendar.tsx
@@ -127,8 +127,6 @@ export function prettifyName(name: string | undefined) {
         .replace("Gauteng Tshwane Group", "Tshwane")
 }
 
-
-
 const getReleaseAssets = async () => {
     const octokit = new Octokit({
         auth: process.env.GH_PAGES_ENV_PAT || process.env.GH_PAGES_PAT
